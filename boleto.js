e19c422562decca5b6843acecee0f4f8127c02f1
WesMattheus <115564950+WesMattheus@users.noreply.github.com>
2023-04-18 21:39:17 -0300

    Update boleto.js

--- boleto.js
+++ boleto.js
@@ -1,19 +1,3 @@
-/*function calcularValor() {
-    var valor = parseFloat(document.getElementById("valor").value);
-    var vencimento = new Date(document.getElementById("vencimento").value);
-    var hoje = new Date();
-    var diasAtraso = Math.floor((hoje - vencimento) / (1000 * 60 * 60 * 24));
-    var valorTotal = valor;
-    
-    if (diasAtraso > 0) {
-      valorTotal += (valor * 0.01 * diasAtraso);
-    } else {
-      valorTotal -= (valor * 1);
-    }
-    
-    document.getElementById("valorTotal").value = valorTotal.toFixed(2);
-  }
-  */
   function verificarPagamento() {
     var numeroBoleto = document.getElementById("numeroBoleto").value;
     var dataVencimento = new Date(document.getElementById("dataVencimento").value);
