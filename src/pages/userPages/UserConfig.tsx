9f2adccfb3fd34f1c55087b410bbcc6fba7bdcf2
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-04-21 22:24:05 -0300

    Add files via upload

---
+++ src/pages/userPages/UserConfig.tsx
@@ -0,0 +1,29 @@
+import { IonPage, IonContent, IonList, IonRouterOutlet } from "@ionic/react";
+
+import {logInOutline, shield} from "ionicons/icons";
+
+import Action from "../../components/Action";
+import ReturnToolbar from "../../components/returnToolbar";
+
+const UserConfig: React.FC = () => {
+  return (
+    <IonPage>
+      <ReturnToolbar title={"Configurações do Usuário"} />
+      <IonContent>
+        <IonList>
+          <Action
+            route={"./TelaPrincipal"}
+            icon={logInOutline}
+            name={"Aterar Login"}
+          />
+          <Action
+            route={"./TelaPrincipal"}
+            icon={shield}
+            name={"Alterar Senha"}
+          />
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+export default UserConfig;
