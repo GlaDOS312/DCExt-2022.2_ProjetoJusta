336844adf15c34123933565305ff9328b8c25e45
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-04-21 22:20:09 -0300

    Add files via upload

---
+++ src/components/SaldoVendas.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { IonCard, IonCardContent, IonCardTitle } from "@ionic/react";
+
+const SaldoVendas: React.FC = () => {
+  return (
+    <IonCard className="card">
+      <IonCardTitle>Card Title</IonCardTitle>
+      <IonCardContent>Here's a small</IonCardContent>
+    </IonCard>
+  );
+};
+export default SaldoVendas;
