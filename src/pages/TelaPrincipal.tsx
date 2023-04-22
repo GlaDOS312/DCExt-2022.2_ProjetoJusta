d9359bb05d867df2d401f837ee5fe1cf973618c4
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-04-21 22:22:59 -0300

    Add files via upload

---
+++ src/pages/TelaPrincipal.tsx
@@ -0,0 +1,49 @@
+import {IonPage,IonContent,IonList,} from "@ionic/react";
+
+import {
+  sendOutline,
+  phonePortraitOutline,
+  receiptOutline,
+  cashOutline,
+} from "ionicons/icons";
+
+import ToolBar from "../components/ToolBar";
+import Action from "../components/Action";
+import SaldoVendas from "../components/SaldoVendas";
+import SaldoCartao from "../components/SaldoCartao";
+
+const TelaPrincipal: React.FC = () => {
+  return (
+    <IonPage>
+      <ToolBar />
+      <IonContent>
+        <SaldoVendas />
+        <IonList>
+          <Action
+            route={"/TransfBanc"}
+            icon={cashOutline}
+            name={"Transferência Bancária"}
+          />
+          <Action
+            route={"/AreaPix"}
+            icon={sendOutline}
+            name={"Área Pix"}
+          />
+          <Action
+            route={"/OpcoesBoleto"}
+            icon={receiptOutline}
+            name={"Pagar e Receber com Boleto"}
+          />
+          <Action
+            route={"/Recarga"}
+            icon={phonePortraitOutline}
+            name={"Recarga de Celular"}
+          />
+        </IonList>
+        <SaldoCartao />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default TelaPrincipal;
