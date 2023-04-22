aca7fe4ead0d3abdc9155dd0f8faff617b64cbdb
Lucas Ribeiro Costa <lucasrios22@hotmail.com>
2023-04-22 17:45:40 -0300

    corrigindo

--- projetojusta/src/App.js
+++ projetojusta/src/App.js
@@ -1,99 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function UserForm() {
-  const [nome, setNome] = useState('');
-  const [sobrenome, setSobrenome] = useState('');
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [conta, setConta] = useState('');
-  const [agencia, setAgencia] = useState('');
-  const [tipo, setTipo] = useState('');
-  const [numero, setNumero] = useState('');
-  const [operadora, setOperadora] = useState('');
+function MyComponent() {
+  const [data, setData] = useState([]);
 
-  const handleSubmit = event => {
-    event.preventDefault();
-
-    const userData = {
-      nome: nome,
-      sobrenome: sobrenome,
-      email: email,
-      senha: senha,
-      conta: conta,
-      agencia: agencia,
-      tipo: tipo,
-      numero: numero,
-      operadora: operadora
-    };
-
-    axios.post('http://localhost:5500/api', userData)
+  useEffect(() => {
+    axios.get('http://localhost:5500/api')
       .then(response => {
-        console.log(response);
+        setData(response.data);
       })
       .catch(error => {
         console.log(error);
       });
-  }
+  }, []);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Nome:
-        <input type="text" value={nome} onChange={event => setNome(event.target.value)} />
-      </label>
-      <br />
-      <label>
-        Sobrenome:
-        <input type="text" value={sobrenome} onChange={event => setSobrenome(event.target.value)} />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input type="email" value={email} onChange={event => setEmail(event.target.value)} />
-      </label>
-      <br />
-      <label>
-        Senha:
-        <input type="password" value={senha} onChange={event => setSenha(event.target.value)} />
-      </label>
-      <br />
-      <label>
-        Conta:
-        <input type="text" value={conta} onChange={event => setConta(event.target.value)} />
-      </label>
-      <br />
-      <label>
-        Agência:
-        <input type="text" value={agencia} onChange={event => setAgencia(event.target.value)} />
-      </label>
-      <br />
-      <label>
-        Tipo:
-        <select value={tipo} onChange={event => setTipo(event.target.value)}>
-          <option value="corrente">Corrente</option>
-          <option value="poupança">Poupança</option>
-        </select>
-      </label>
-      <br />
-      <label>
-        Número:
-        <input type="text" value={numero} onChange={event => setNumero(event.target.value)} />
-      </label>
-      <br />
-      <label>
-        Operadora:
-        <select value={operadora} onChange={event => setOperadora(event.target.value)}>
-          <option value="oi">Oi</option>
-          <option value="tim">Tim</option>
-          <option value="vivo">Vivo</option>
-          <option value="claro">Claro</option>
-        </select>
-      </label>
-      <br />
-      <button type="submit">Cadastrar</button>
-    </form>
+    <div>
+      {data.map(user => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
-export default UserForm;
+export default MyComponent;
