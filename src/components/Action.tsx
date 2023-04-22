25a109d59fbb5b0ebca1d24b910b8fc94eb77961
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-04-21 22:19:03 -0300

    Add files via upload

---
+++ src/components/Action.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { useHistory } from "react-router";
+import { LocationDescriptor } from "history";
+import { IonButton, IonIcon, IonItem} from "@ionic/react";
+
+
+const Action: React.FC<{
+  icon: string | undefined;
+  name: string | undefined;
+  route: LocationDescriptor;
+}> = props => {
+  const history = useHistory();
+  
+  return (
+    <IonItem>
+      <IonButton
+        onClick={() => history.push(props.route)}
+        className="block"
+        fill="clear"
+      >
+        <IonIcon slot="start" icon={props.icon}></IonIcon>
+        {props.name}
+      </IonButton>
+    </IonItem>
+  );
+};
+export default Action;
