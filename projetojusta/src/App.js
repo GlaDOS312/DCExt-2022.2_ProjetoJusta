221bfa4c3acb66bf98191a9be05316b9e3e97562
Paulinho081 <paulofs2104@gmail.com>
2023-04-24 03:03:00 -0300

    Sidebar

--- projetojusta/src/App.js
+++ projetojusta/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Navbar from '.Componentes/Navbar';
-import { BrowserRouter as Router, switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, stch, Route} from 'react-router-dom';
 
 function App() {
     return (
         <div>
             <Router>
                  <Navbar />
-                 <switch>
+                 <Switch>
                     <Route path='/' />
-                 </switch>
+                 </Switch>
             </Router>
            
         </div>
