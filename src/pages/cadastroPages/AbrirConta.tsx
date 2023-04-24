6ab88c07460ae909b3c7835738aa6000d01ec221
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-04-24 03:06:54 -0300

    Add files via upload

---
+++ src/pages/cadastroPages/AbrirConta.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  IonButton,
+  IonContent,
+  IonPage,
+} from "@ionic/react";
+import { useHistory } from "react-router";
+
+import ReturnToolbar from "../../components/returnToolbar";
+
+
+const AbrirConta: React.FC = () => {
+  const history = useHistory();
+  return (
+    <div className="estilo">
+      <IonPage>
+        <ReturnToolbar title={"Abrir Conta"}/>
+        <IonContent className="padrao">
+          <IonButton
+            shape="round"
+            className="botaobranco"
+            onClick={() => history.push("/comecar")}
+          >
+            Começar
+          </IonButton>
+
+          <IonButton
+            shape="round"
+            className="botaopreto"
+            onClick={() => history.push("/login")}
+          >
+            Concluir cadastro
+          </IonButton>
+        </IonContent>
+      </IonPage>
+    </div>
+  );
+};
+export default AbrirConta;
