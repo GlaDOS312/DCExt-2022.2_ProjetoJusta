e942eaa550502c75496871e07b33c2c540666e5e
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-04-24 02:20:56 -0300

    Update TransfPix.tsx

--- src/pages/pixPages/TransfPix.tsx
+++ src/pages/pixPages/TransfPix.tsx
@@ -17,30 +17,29 @@ import { useRef, useState } from "react";
 import ReturnToolbar from "../../components/returnToolbar";
 
 const TransfPix: React.FC = () => {
-  
   const [tipoChave, setTipoChave] = useState<string>();
   const [error, setError] = useState<string>();
 
   const valorRef = useRef <HTMLIonInputElement>(null);
   const chaveRef = useRef <HTMLIonInputElement>(null);
 
-  const EnviarPix = () => {
+  const enviarPix = () => {
 
-    let valor = valorRef.current!.value;
-    let chave = chaveRef.current!.value;
+  let valor = valorRef.current!.value;
+  let chave = chaveRef.current!.value;
 
-    if (!valor || !chave){
-      setError("Preencher corretamente antes de continuar");
-       return;
-    } else if (+valor < 0.01) {
-      setError("Não é possível transferir esse valor");
-      return;
-    }   
-  }
+  if (!tipoChave || !valor || !chave){
+    setError("Preencher corretamente antes de continuar");
+    return;
+  } else if (+valor < 0.01) {
+    setError("Não é possível transferir esse valor");
+    return;
+  }   
+};
     
-  const inputChangeHandler = (event:CustomEvent) => {
-    setTipoChave(event.detail.value)
-  }
+const inputChangeHandler = (event: CustomEvent) => {
+  setTipoChave(event.detail.value);
+};
 
   return (
     <IonPage>
@@ -61,25 +60,25 @@ const TransfPix: React.FC = () => {
         <IonGrid className="ion-text-center ion-margin">
           <IonRow>
             <IonCol>
-              <IonSelect
-                onIonChange={inputChangeHandler}
-                aria-label="chave"
-                interface="popover"
-                placeholder="Selecione o Tipo de Chave"
-              >
-                <IonSelectOption value="Telefone">telefone</IonSelectOption>
-                <IonSelectOption value="CPF">CPF</IonSelectOption>
-                <IonSelectOption value="CNPJ">CNPJ</IonSelectOption>
-                <IonSelectOption value="E-mail">E-mail</IonSelectOption>
-              </IonSelect>
+              <IonItem>
+                <IonSelect
+                  onIonChange={inputChangeHandler}
+                  aria-label="chave"
+                  interface="popover"
+                  placeholder="Selecione o Tipo de Chave"
+                >
+                  <IonSelectOption value="Telefone">Telefone</IonSelectOption>
+                  <IonSelectOption value="CPF">CPF</IonSelectOption>
+                  <IonSelectOption value="CNPJ">CNPJ</IonSelectOption>
+                  <IonSelectOption value="E-mail">E-mail</IonSelectOption>
+                </IonSelect>
+              </IonItem>
             </IonCol>
           </IonRow>
           <IonRow>
             <IonCol>
               <IonItem>
-                <IonLabel position="floating">
-                  Digite o valor da Transferência
-                </IonLabel>
+                <IonLabel position="floating">Valor da Transferência</IonLabel>
                 <IonInput
                   ref={valorRef}
                   placeholder="R$ 0,00"
@@ -91,7 +90,9 @@ const TransfPix: React.FC = () => {
           <IonRow>
             <IonCol>
               <IonItem>
-                <IonLabel position="floating">{tipoChave}</IonLabel>
+                <IonLabel position="floating">
+                  {tipoChave === undefined ? "Chave PIX" : tipoChave}
+                </IonLabel>
                 <IonInput
                   ref={chaveRef}
                   type={tipoChave === "E-mail" ? "text" : "number"}
@@ -99,10 +100,14 @@ const TransfPix: React.FC = () => {
               </IonItem>
             </IonCol>
           </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonButton className="btn" fill="clear" onClick={enviarPix}>
+                Continuar
+              </IonButton>
+            </IonCol>
+          </IonRow>
         </IonGrid>
-        <IonButton onClick={EnviarPix} fill="outline">
-          Continuar
-        </IonButton>
       </IonContent>
     </IonPage>
   );
