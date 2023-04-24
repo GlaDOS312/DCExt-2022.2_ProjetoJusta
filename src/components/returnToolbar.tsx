6287df08ee645ece710723ffecaa193a81b8da89
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-04-24 02:30:23 -0300

    Update returnToolbar.tsx

--- src/components/returnToolbar.tsx
+++ src/components/returnToolbar.tsx
@@ -1,23 +1,27 @@
-import React from "react";
+import * as React from "react";
 import { useHistory } from "react-router";
-import { IonButton, IonHeader, IonIcon, IonTitle, IonToolbar } from "@ionic/react";
-import {arrowBackOutline} from "ionicons/icons";
-
+import { LocationDescriptor } from "history";
+import {
+  IonButton,
+  IonIcon,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { arrowBackOutline } from "ionicons/icons";
 
 const ReturnToolbar: React.FC<{
-    title: string|undefined;
-}> = props => {
+  title: string | undefined;
+  route: LocationDescriptor;
+}> = (props) => {
   const history = useHistory();
-  
-    return (
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle className="title">{props.title}</IonTitle>
-            <IonButton onClick={() => history.push("/TelaPrincipal")} fill="clear">
-              <IonIcon icon={arrowBackOutline}></IonIcon>
-            </IonButton>
-        </IonToolbar>
-      </IonHeader>
-    );
+
+  return (
+      <IonToolbar>
+        <IonTitle className="title">{props.title}</IonTitle>
+        <IonButton onClick={() => history.push(props.route)} fill="clear">
+          <IonIcon icon={arrowBackOutline}></IonIcon>
+        </IonButton>
+      </IonToolbar>
+  );
 };
 export default ReturnToolbar;
