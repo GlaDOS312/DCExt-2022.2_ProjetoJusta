bc9ad5246c571f03c466f4ef0ce2e787b6d05cad
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-04-24 03:15:30 -0300

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,52 +1,50 @@
-import { IonRouterOutlet, IonInput, IonItem, IonButton, IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-//import { add } from 'ionicons/icons';
-import { Redirect, Route } from 'react-router-dom';
-import { IonReactRouter } from '@ionic/react-router';
-import TelaPrincipal from './TelaPrincipal';
-import { useHistory } from 'react-router';
-import { RouteComponentProps} from 'react-router';
-import './Estilo.css';
+import {
+  IonInput,
+  IonItem,
+  IonButton,
+  IonContent,
+  IonPage,
+} from "@ionic/react";
+import React from "react";
+import { useHistory } from "react-router";
+import { RouteComponentProps } from "react-router";
+import ReturnToolbar from "../components/returnToolbar";
 
 
 const Login: React.FC<RouteComponentProps> = () => {
-    const history = useHistory();
+  const history = useHistory();
   return (
     <div className="estilo">
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-          <IonBackButton defaultHref="/login" />
-          </IonButtons>
-          <IonTitle>Login</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <IonPage>
+        <ReturnToolbar title={"Login"}/>
 
-      <IonContent>
-        
-        <div className="container">
-        <IonItem>
-          <IonInput label="E-mail" type="email" placeholder="Digite o seu e-mail"></IonInput>
-        </IonItem>
-        <IonItem>
-          <IonInput label="Digite a sua senha" type="password" value="password"></IonInput>
-        </IonItem>
-  
+        <IonContent>
+          <div className="container">
+            <IonItem>
+              <IonInput
+                label="E-mail"
+                type="email"
+                placeholder="Digite o seu e-mail"
+              ></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonInput
+                label="Digite a sua senha"
+                type="password"
+                value="password"
+              ></IonInput>
+            </IonItem>
 
-        <IonReactRouter>
-          <IonRouterOutlet>
-            <Route path="/telaprincipal" component={TelaPrincipal} />
-            <Redirect exact from="/" to="/login" />
-          </IonRouterOutlet>
-        </IonReactRouter>
-
-
-        <IonButton  shape="round" className="botaobranco" onClick={() => history.push('/telaprincipal')}>Entrar</IonButton>
-
-        </div>
-      </IonContent>
-    </IonPage>
+            <IonButton
+              shape="round"
+              className="botaobranco"
+              onClick={() => history.push("/telaprincipal")}
+            >
+              Entrar
+            </IonButton>
+          </div>
+        </IonContent>
+      </IonPage>
     </div>
   );
 };
