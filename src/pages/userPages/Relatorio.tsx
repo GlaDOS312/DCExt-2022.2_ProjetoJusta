ca3c852f150ed9a8b1a21c4f2d09f4c6434668d6
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-04-27 04:15:18 -0300

    Update Relatorio.tsx

--- src/pages/userPages/Relatorio.tsx
+++ src/pages/userPages/Relatorio.tsx
@@ -1,27 +1,45 @@
-import React from "react";
-import sales from "./salesData";
-import { IonCol, IonContent, IonGrid,IonPage, IonRow, IonItem } from "@ionic/react";
+import React, { useState } from "react";
+import { IonContent, IonGrid,IonPage,IonItem,IonSelect, IonSelectOption } from "@ionic/react";
+
 import ReturnToolbar from "../../components/returnToolbar";
+import RelSegment from "../../components/RelSegment";
+
+import { creditTable, debitTable } from "./Tabelas";
+
 
 const Relatorio: React.FC = () => {
-  const data = sales;
-  let count = data.length
+  const [filtro, setFiltro] = useState<number>(30)
+  const [opc, setOpc] = useState<"credito"|"debito">("credito");
+
   return (
     <IonPage>
       <ReturnToolbar title={"Relatória de Vendas"} />
       <IonContent>
-        <IonGrid>
-          <IonRow className="header-row">
-            <IonCol size="4">
-              <IonItem>Data</IonItem>
-            </IonCol>
-            <IonCol size="4">
-              <IonItem>Total de Vendas</IonItem>
-            </IonCol>
-            <IonCol size="4">
-              <IonItem>Brandeira</IonItem>
-            </IonCol>
-          </IonRow>
+        <div>
+          <IonItem>
+            <label>FILTRO: </label>
+            <IonSelect
+              onIonChange={(event: CustomEvent) =>
+                setFiltro(event.detail.value)
+              }
+              interface="popover"
+              placeholder="Todos"
+            >
+              <IonSelectOption value="5">Últimos 5 dias</IonSelectOption>
+              <IonSelectOption value="10">Últimos 10 dias</IonSelectOption>
+              <IonSelectOption value="15">Últimos 15 dias</IonSelectOption>
+              <IonSelectOption value="30">Últimos 30 dias</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+        </div>
+        <div>
+          <RelSegment
+            selected={opc}
+            onSelect={(selected: "credito" | "debito") => setOpc(selected)}
+          />
+        </div>
+        <IonGrid className="my-table">
+          {opc === "credito" ? creditTable : debitTable}
         </IonGrid>
       </IonContent>
     </IonPage>
