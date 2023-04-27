c710e023b9111fab79eff0d5b6c66d7b80b940a1
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-04-27 04:11:03 -0300

    Update Recarga.tsx

--- src/pages/recargaPages/Recarga.tsx
+++ src/pages/recargaPages/Recarga.tsx
@@ -11,39 +11,49 @@ import {
   IonItem,
   IonLabel,
   IonInput,
+  IonCard,
 } from "@ionic/react";
 
-import { useRef, useState } from "react";
+import {useRef, useState } from "react";
 
 import ReturnToolbar from "../../components/returnToolbar";
+import { useHistory } from "react-router";
 
 
 const Recarga: React.FC = () => {
-  const [operadora, setOperadora] = useState<string>();
+  const history = useHistory();
   const [error, setError] = useState<string>();
 
+  const [operadora, setOperadora] = useState<string>();
+  
   const telefoneRef = useRef<HTMLIonInputElement>(null);
-  const valorRef = useRef<HTMLIonInputElement>(null);
+  const valorRef= useRef<HTMLIonInputElement>(null);
 
   
-  const recarga = () => {
-
+  const handleSubmit = () => {
     let telefone = telefoneRef.current!.value;
-    let valor = telefoneRef.current!.value;
+    let valor = valorRef.current!.value;
+    var Cashback;
 
     if (!operadora || !valor || !telefone) {
       setError("Preencher corretamente antes de continuar");
       return;
-    } else if (+valor < 5) {
-      setError("Não é possível recarregar esse valor");
-      return;
-    }  
+    } else if(+valor < 10){
+        setError("Não é possível recarregar esse valor");
+        return;
+    }
+    if(+valor >= 30){
+      Cashback = (+valor * 0.05).toFixed(2);
+    } else if(+valor >= 50){
+      Cashback = (+valor * 0.1).toFixed(2);
+    } else if(+valor >= 100){
+      Cashback = (+valor * 0.15).toFixed(2);
+    }else{
+      Cashback = 0;
+    }
+    history.push(`./RecargaSucess?valor=${valor}&cashback=${Cashback}&telefone=${telefone}`)
+    console.log(operadora, valor, Cashback, telefone)
   };
-
-const inputChangeHandler = (event:CustomEvent) => {
-  setOperadora(event.detail.value);
-}
-
 
 
   return (
@@ -60,14 +70,21 @@ const inputChangeHandler = (event:CustomEvent) => {
           },
         ]}
       />
-      <ReturnToolbar title={"Recarga de Celular"} route={"./AreaPix"}/>
+      <ReturnToolbar title={"Recarga de Celular"} />
       <IonContent>
+        <IonCard className="card">
+          <p>Valor Mínimo para Recarga: R$ 10,00</p>
+          <br />
+          <p>Recargas Acima de R$ 30,00: 5% de Cashback!</p>
+          <p>Recargas Acima de R$ 50,00: 10% de Cashback!</p>
+          <p>Recargas Acima de R$ 100,00: 15% de Cashback!</p>
+        </IonCard>
         <IonGrid>
           <IonRow>
             <IonCol>
               <IonItem>
                 <IonSelect
-                  onIonChange={inputChangeHandler}
+                  onIonChange={(event:CustomEvent) => setOperadora(event.detail.value)}
                   aria-label="Operadora"
                   interface="popover"
                   placeholder="Selecionar Operadora"
@@ -86,7 +103,11 @@ const inputChangeHandler = (event:CustomEvent) => {
                 <IonLabel position="floating">
                   Número de Telefone (com DDD)
                 </IonLabel>
-                <IonInput ref={telefoneRef} type="number"></IonInput>
+                <IonInput
+                  ref={telefoneRef}
+                  type="number"
+                  placeholder="(88)88888-888"
+                ></IonInput>
               </IonItem>
             </IonCol>
           </IonRow>
@@ -96,15 +117,15 @@ const inputChangeHandler = (event:CustomEvent) => {
                 <IonLabel position="floating">Valor da Recarga</IonLabel>
                 <IonInput
                   ref={valorRef}
-                  placeholder="R$ 0,00"
                   type="number"
+                  placeholder="R$ 0,00"
                 ></IonInput>
               </IonItem>
             </IonCol>
           </IonRow>
           <IonRow>
-            <IonCol>
-              <IonButton className="btn" fill="clear" onClick={recarga}>
+            <IonCol className="ioncol">
+              <IonButton className="btn" fill="clear" onClick={handleSubmit}>
                 Continuar
               </IonButton>
             </IonCol>
