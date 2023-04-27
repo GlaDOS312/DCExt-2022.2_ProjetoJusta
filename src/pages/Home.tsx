998d98e7f94f7f894b0c37b6fa95dd52326481a1
Jessicamsza <121313732+Jessicamsza@users.noreply.github.com>
2023-04-27 17:35:49 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,37 @@
-import {
-  IonButton,
-  IonIcon,
-  IonContent,
-  IonPage,
-} from "@ionic/react";
+import { IonButton, IonIcon, IonContent, IonPage } from "@ionic/react";
 import { useHistory } from "react-router";
 
-import Justalogo from "./justalogo.jpeg";
-import { chatbubble } from 'ionicons/icons';
+import { chatbubble } from "ionicons/icons";
 
-const Home: React.FC= () => {
+import Justalogo from "./Justalogo.jpeg";
+
+const Home: React.FC = () => {
   const history = useHistory();
   return (
-    <IonPage> 
+    <IonPage>
       <IonContent>
         <div className="container">
-          <img className ="img" src = {Justalogo} alt="logo Justa"/>
-          <IonButton  shape="round" className="botaobranco" onClick={() => history.push('/login')}>Login</IonButton>
+          <img className="img" src={Justalogo} alt="logo Justa" />
+          <IonButton
+            shape="round"
+            className="botaobranco"
+            onClick={() => history.push("/login")}
+          >
+            Login
+          </IonButton>
 
-   
-          <IonButton  shape="round" className="botaoazul" onClick={() => history.push('/abrirconta')}>Abrir conta</IonButton>
+          <IonButton
+            shape="round"
+            className="botaoazul"
+            onClick={() => history.push("/abrirconta")}
+          >
+            Abrir conta
+          </IonButton>
         </div>
 
         <IonIcon className="icone" icon={chatbubble}></IonIcon>
-    
-      
-
       </IonContent>
     </IonPage>
-  
   );
 };
 export default Home;
